021b9aae27a9545dd007ed6f6eaf736fcead1f72
Abdulloh <abdullajonmamnazirov@gmail.com>
2023-05-07 14:44:59 +0500

    bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import App from './App'
+
+// css
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
