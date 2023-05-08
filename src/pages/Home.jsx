8e865e8638359dbd931e6196a51828fa3f72ecfb
Abdulloh <abdullajonmamnazirov@gmail.com>
2023-05-08 21:20:14 +0500

    navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,49 @@
 import React from 'react'
 import HomeCard from '../components/HomeCard'
 import Sidebar from '../components/Sidebar'
-import { BarElement } from "chart.js"
+import { Bar } from "react-chartjs-2";
+import { Chart } from 'chart.js/auto';
+import Navbar from '../components/Navbar';
 
 const Home = () => {
-  let bar = new BarElement()
-  console.log(bar);
+
+  // Bar Chart
+  const labels = ["January", "February", "March", "April", "May"];
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Jami o'quvchilar",
+        backgroundColor: "rgb(50, 153, 249)",
+        borderColor: "rgb(255, 99, 132)",
+        data: [170, 200, 185, 240, 280, 300],
+      },
+      {
+        label: "Tark etganlar",
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+        data: [25, 20, 12, 25, 40, 48],
+      },
+    ],
+  };
   return (
-    <div className='d-flex'>
-      <Sidebar/>
-      <main className='homeMainSide'>
-        <div className="homeCardsWrapper p-5 w-100">
-          <HomeCard title="Jami o'quvchilar soni:" number="255"/>
-          <HomeCard title="O'qituvchilar soni:" number="13"/>
-          <HomeCard title="Shu oy tark etganlar:" number="31"/>
-          <HomeCard title="Jami guruhlar soni:" number="26"/>
+    <div className="d-flex">
+      <Sidebar />
+      <main className="homeMainSide">
+        <Navbar title="Xisobot"/>
+        <div className="homeCardsWrapper p-5">
+          <HomeCard title="Jami o'quvchilar soni:" number="255" />
+          <HomeCard title="O'qituvchilar soni:" number="13" />
+          <HomeCard title="Shu oy tark etganlar:" number="31" />
+          <HomeCard title="Jami guruhlar soni:" number="26" />
+        </div>
+        <div>
+          <h2 className='text-center'>Aprel oyi bo'yicha statistika</h2>
+          <Bar data={data} />
         </div>
       </main>
     </div>
-  )
+  );
 }
 
 export default Home
