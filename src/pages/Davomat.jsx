eddf17555865de8c866d3581ec147a5968f41d11
ziyadem <maxkamovaziyaem17@gmail.com>
2023-05-09 00:30:54 +0500

    davomat

--- src/pages/Davomat.jsx
+++ src/pages/Davomat.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import Sidebar from "../components/Sidebar";
+import CardLesson from "../components/CardLesson";
+
 
 const Davomat = () => {
   return (
-    <div>Davomat</div>
-  )
+    <div className="d-flex">
+      <Sidebar />
+      <div className="sd-right w-100">
+        <div className="nav">bla bla</div>
+        <div className="davomat pt-4 ps-5 pe-5">
+          <div className="d-flex justify-content-between pb-4">
+            <h2 className="dv-hedding fw-bold fs-2">
+              Davomat olinadigan guruhni tanlang
+            </h2>
+            <input
+              type="search"
+              className="form-control rounded-4 dv-search"
+              placeholder="Guruh nomini kiriting"
+            />
+          </div>
+          <div className="d-flex flex-wrap justify-content-between">
+            <CardLesson />
+            <CardLesson />
+            <CardLesson />
+            <CardLesson />
+            <CardLesson />
+            <CardLesson />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Davomat
