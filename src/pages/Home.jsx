f476428e54080c3de25389476b1fe4301a3ae1a8
Abdulloh <abdullajonmamnazirov@gmail.com>
2023-05-08 20:43:55 +0500

    home cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import HomeCard from '../components/HomeCard'
 import Sidebar from '../components/Sidebar'
+import { BarElement } from "chart.js"
 
 const Home = () => {
+  let bar = new BarElement()
+  console.log(bar);
   return (
     <div className='d-flex'>
       <Sidebar/>
-      Home
+      <main className='homeMainSide'>
+        <div className="homeCardsWrapper p-5 w-100">
+          <HomeCard title="Jami o'quvchilar soni:" number="255"/>
+          <HomeCard title="O'qituvchilar soni:" number="13"/>
+          <HomeCard title="Shu oy tark etganlar:" number="31"/>
+          <HomeCard title="Jami guruhlar soni:" number="26"/>
+        </div>
+      </main>
     </div>
   )
 }
